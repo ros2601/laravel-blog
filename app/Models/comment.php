4f18d88db320252e00b0d4f5e7abdf16353330e3
ros2601 <112537398+ros2601@users.noreply.github.com>
2023-01-20 12:18:43 +0530

    Blog project

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class comment extends Model
 {
@@ -15,8 +16,8 @@ class comment extends Model
     {
         return $this->belongsTo(upload::class);
     }
-    public function user()
+   public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo('App\Models\User','user_id');
     }
 }
