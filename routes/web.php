4982c2dc06aeacfac609ad1aa9f7ac6ccd4135eb
ros2601 <112537398+ros2601@users.noreply.github.com>
2023-02-06 09:00:18 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
  
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomAuthController;
- 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
+
 Route::get('/', function () {
    return view('welcome');
 });
@@ -18,17 +19,30 @@ Route::get('/upload', function () {
 Route::get('/contact', function () {
    return view('contact');
 });
-// Route::get('/', [CustomAuthController::class, 'home']); 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('postlogin', [CustomAuthController::class, 'login'])->name('postlogin'); 
-Route::get('signup', [CustomAuthController::class, 'signup'])->name('register-user');
-Route::post('postsignup', [CustomAuthController::class, 'signupsave'])->name('postsignup'); 
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
-Route::post('/upload1',"App\Http\Controllers\CustomAuthController@upload");
-Route::get('/home',"App\Http\Controllers\CustomAuthController@products");
-Route::get('/detail/{id}',"App\Http\Controllers\CustomAuthController@detail");
-Route::post('/comm/{id}',"App\Http\Controllers\CustomAuthController@comments");
-Route::post('/contact1',"App\Http\Controllers\CustomAuthController@contact");
-Route::get('/user',"App\Http\Controllers\CustomAuthController@user");
-Route::get('/delete/{id}',"App\Http\Controllers\CustomAuthController@delete");
+
+// Route::post('postlogin', [AuthController::class, 'login'])->name('postlogin'); 
+Route::get('signup', [AuthController::class, 'signup'])->name('register-user');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+
+
+//----------------------for register form---------------------------------------------------
+Route::post('postsignup', [AuthController::class, 'signupsave'])->name('postsignup'); 
+//----------------------for login form---------------------------------------------------
+Route::post('postsignup', [AuthController::class, 'signupsave'])->name('postsignup'); 
+//----------------------for logout---------------------------------------------------
+Route::get('signout', [AuthController::class, 'signOut'])->name('signout');
+
+//-----------------for uploading blogs by auth user-------------------------------------
+Route::post('/upload1',"App\Http\Controllers\BlogController@upload");
+//-------------------shows blogs on home page------------------------------------------
+Route::get('/home',"App\Http\Controllers\BlogController@blogs");
+//------------------for showing a particular post with details-------------------------
+Route::get('/detail/{id}',"App\Http\Controllers\BlogController@detail");
+//----------------------for comment---------------------------------------------------
+Route::post('/comm/{id}',"App\Http\Controllers\BlogController@comments");
+//--------------------for sending a query-------------------------------------------------
+Route::post('/contact1',"App\Http\Controllers\BlogController@contact");
+//----------------for opening page that shows posts by auth user-----------------------
+Route::get('/user',"App\Http\Controllers\BlogController@user");
+//---------------------for deleting a post-----------------------------------------------
+Route::get('/delete/{id}',"App\Http\Controllers\BlogController@delete");
